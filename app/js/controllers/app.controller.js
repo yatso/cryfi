967caf829adec18d53de41c0c72d3f1b640cb783
Thuong Ho <thuong.t.ho@gmail.com>
2016-06-25 23:45:40 -0700

    add shitty display to home with upvote

--- app/js/controllers/app.controller.js
+++ app/js/controllers/app.controller.js
@@ -5,20 +5,35 @@
         .module('cryfi')
         .controller('AppController', AppController);
 
-    AppController.$inject = ['$scope', 'CafeService'];
+    AppController.$inject = ['$scope', '$http', '$firebaseObject', 'FIREBASE_URL', 'firebaseService', '$timeout', 'CafeService'];
 
     /* @ngInject */
-    function AppController($scope, CafeService) {
+    function AppController($scope, $http, $firebaseObject, FIREBASE_URL, firebaseService, $timeout, CafeService) {
         // var vm = this;
         // vm.title = 'Controller';
         $scope.title = "CryFi";
+        $scope.upvote = upvote;
 
-        activate();
+        $timeout(function() {
+            activate();
+        }, 10);
+        
 
         ////////////////
 
+        var _fbService = firebaseService;
+
         function activate() {
-//            console.log(JSON.stringify(CafeService.cafes()));
+            var _promise = _fbService.retrieveCafes();
+
+            _promise.then(function(result) {
+                $scope.yelpCafeKeys = Object.keys(result.data);
+                $scope.yelpCafes = result.data;
+            });
+        }
+
+        function upvote (id) {
+            _fbService.upvote(id);
         }
     }
 })();
